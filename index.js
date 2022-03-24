6f15ad52203d8a1abff6057c4588a33a9e332ee5
Eddy Bernardeau <eddybernardeau@gmail.com>
2022-03-24 11:14:24 +0100

    #majeur

--- index.js
+++ index.js
@@ -1,56 +1,6 @@
 const express = require('express');
-const md5 = require('md5');
+var router = require('./router');
+
 const app = express()
-const port = 2900
 
-const userRepository = require('./user-repository');
-
-app.use(express.json());
-
-app.use((req, res, next) => {
-   console.log(`request called at : ${new Date()}`);
-   next();
-});
-
-app.use((err,  req, res, next) => {});
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.send("Bleg");
-});
-
-app.get('/users', (req, res) => {
-    console.log(`request called at : ${new Date()}, ip:${req.ip}, time:, method:${req.method},route:${req.route}`);
-    res.send(userRepository.getUser());
-});
-
-app.get('/users/:firstname', (req, res) => {
-    res.send(userRepository.getUserByFirstname(req.params.firstname));
-});
-
-app.get('/users/Id/:id', (req, res) => {
-    res.send(userRepository.getUserById(req.params.id));
-});
-
-app.post('/users', (req, res) => {
-    let newUser = req.body
-    newUser.password = md5(req.body.password)
-    userRepository.createUser(newUser)
-    res.statusCode = 201
-    res.send('Utilisateur créé\n');
-});
-
-app.delete('/delete-user/:id', (req,res) => {
-    userRepository.deleteUser(req.params.id)
-    res.send(`Utilisateur ${req.params.id} a été supprimé`)
-})
-
-app.put('/update-user/:id', (req,res) => {
-    userRepository.updateUser(req.body.id, req.body)
-    res.send(`Utilisateur ${req.body.id} mis à jour`)
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-});
+app.use('/router', router);
---
+++ router.js
@@ -0,0 +1,57 @@
+const express = require('express');
+var router = express.Router();
+const md5 = require('md5');
+var responseTime = require('response-time')
+const port = 2900
+
+const userRepository = require('./user-repository');
+
+router.use(express.json());
+router.use(responseTime())
+
+router.use((req, res, next) => {
+   console.log(`request called at : ${new Date()}`);
+   next();
+});
+
+router.get('/', (req, res) => {
+    res.send("Bleg");
+});
+
+router.get('/users', (req, res) => {
+    console.log(`request called at : ${new Date()}, ip:${req.ip}, time:, method:${req.method}, path:${req.path}`);
+    res.send(userRepository.getUser());
+});
+
+router.get('/users/:firstname', (req, res) => {
+    console.log(`request called at : ${new Date()}, ip:${req.ip}, time:, method:${req.method}, path:${req.path}`);
+    res.send(userRepository.getUserByFirstname(req.params.firstname));
+});
+
+router.get('/users/Id/:id', (req, res) => {
+    console.log(`request called at : ${new Date()}, ip:${req.ip}, time:, method:${req.method}, path:${req.path}`);
+    res.send(userRepository.getUserById(req.params.id));
+});
+
+router.post('/users', (req, res) => {
+    console.log(`request called at : ${new Date()}, ip:${req.ip}, time:, method:${req.method}, path:${req.path}`);
+    let newUser = req.body
+    newUser.password = md5(req.body.password)
+    userRepository.createUser(newUser)
+    res.statusCode = 201
+    res.send('Utilisateur créé\n');
+});
+
+router.delete('/delete-user/:id', (req,res) => {
+    console.log(`request called at : ${new Date()}, ip:${req.ip}, time:, method:${req.method}, path:${req.path}`);
+    userRepository.deleteUser(req.params.id)
+    res.send(`Utilisateur ${req.params.id} a été supprimé`)
+})
+
+router.put('/update-user/:id', (req,res) => {
+    console.log(`request called at : ${new Date()}, ip:${req.ip}, time:, method:${req.method}, path:${req.path}`);
+    userRepository.updateUser(req.body.id, req.body)
+    res.send(`Utilisateur ${req.body.id} mis à jour`)
+})
+
+module.exports = router
